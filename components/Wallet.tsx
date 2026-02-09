1ffa0df096df89b077679763a9b28fd33a3850ed
Udit Kundu <udit@Udits-MacBook-Air.local>
2026-02-09 19:48:03 +0530

    improved ui realted to showing keys and copy

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -1,4 +1,9 @@
-import { Delete } from "lucide-react";
+"use client";
+
+import { Copy, Delete, Trash2Icon } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
+import AlertPrivateKeyVisible from "./AlertPrivateKeyVisible";
 
 interface WalletProps {
   privateKey: string;
@@ -6,22 +11,96 @@ interface WalletProps {
 }
 
 export default function Wallet({ publicKey, privateKey }: WalletProps) {
+  const [fullPublicKeyVisible, setFullPublicKeyVisible] = useState(false);
+  const [fullPrivateKeyVisible, setFullPrivateKeyVisible] = useState(false);
+
+  const [showAlertPrivateKeyModal, setShowAlertPrivateKeyModal] =
+    useState(false);
+
+  const visiblePublicKeyOnUI =
+    publicKey.slice(0, 6) + "..." + publicKey.slice(-6);
+
+  const visiblePrivateKeyOnUI =
+    privateKey.slice(0, 6) + "..." + privateKey.slice(-6);
+
+  const handleCopy = (key: "publicKey" | "privateKey") => {
+    key === "publicKey"
+      ? navigator.clipboard.writeText(publicKey)
+      : navigator.clipboard.writeText(privateKey);
+
+    toast.success(
+      `${key === "publicKey" ? "Public key" : "Private key"} copied to clipboard`
+    );
+  };
+
   return (
     <div className="flex flex-col gap-2 rounded-md border-accent border-2">
+      <AlertPrivateKeyVisible
+        showModal={showAlertPrivateKeyModal}
+        setShowModal={setShowAlertPrivateKeyModal}
+        setFullPrivateKeyVisible={setFullPrivateKeyVisible}
+      />
       <div className="flex justify-between items-center p-4">
         <p className="text-2xl md:text-4xl font-bold">Wallet 1</p>
-        <Delete size={16} color="red" />
+
+        <Trash2Icon
+          size={20}
+          color="red"
+          className="cursor-pointer hover:opacity-70 transition-all duration-300"
+        />
       </div>
       <div className="bg-accent flex flex-col gap-6 p-4 rounded-t-2xl">
+        {/* Public key */}
         <div className="flex flex-col gap-1">
           <p className="text-lg md:text-2xl font-bold">Public Key</p>
-          <p className="text-sm md:text-lg opacity-70 truncate">{publicKey}</p>
+          <div className="flex justify-between items-center gap-8">
+            <p
+              className="text-sm md:text-lg opacity-70 break-all cursor-pointer hover:opacity-100 transition-all duration-300"
+              onClick={() => {
+                setFullPublicKeyVisible(!fullPublicKeyVisible);
+                handleCopy("publicKey");
+              }}
+            >
+              {fullPublicKeyVisible ? publicKey : visiblePublicKeyOnUI}
+            </p>
+
+            <div>
+              <Copy
+                size={20}
+                height={20}
+                className="cursor-pointer opacity-70 hover:opacity-100 transition-all duration-300"
+                onClick={() => handleCopy("publicKey")}
+              />
+            </div>
+          </div>
         </div>
+        {/* Private key */}
         <div className="flex flex-col gap-1">
           <p className="text-lg md:text-2xl font-bold">Private Key</p>
-          <p className="text-sm md:text-lg opacity-70 break-all">
-            {privateKey}
-          </p>
+          <div className="flex justify-between items-start gap-8">
+            <p
+              className="text-sm md:text-lg opacity-70 break-all cursor-pointer hover:opacity-100 transition-all duration-300 "
+              onClick={() => {
+                handleCopy("privateKey");
+
+                if (!fullPrivateKeyVisible) {
+                  setShowAlertPrivateKeyModal(true);
+                } else {
+                  setFullPrivateKeyVisible(false);
+                }
+              }}
+            >
+              {fullPrivateKeyVisible ? privateKey : visiblePrivateKeyOnUI}
+            </p>
+
+            <div>
+              <Copy
+                size={20}
+                className="cursor-pointer opacity-70 hover:opacity-100 transition-all duration-300"
+                onClick={() => handleCopy("privateKey")}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
