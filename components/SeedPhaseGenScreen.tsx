4739c54971af0fdaced0d9d8366555fa9d123822
Udit Kundu <udit@Udits-MacBook-Air.local>
2026-02-09 11:20:37 +0530

    init -- generate seedphase screen done

---
+++ components/SeedPhaseGenScreen.tsx
@@ -0,0 +1,44 @@
+import { seedPhaseGen } from "@/app/utils/wallet";
+import { Button } from "@/components/ui/button";
+import { Field, FieldDescription, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+interface SeedPhaseGenScreenProps {
+  setSeedPhase: React.Dispatch<React.SetStateAction<string | null>>;
+}
+
+export default function SeedPhaseGenScreen({
+  setSeedPhase,
+}: SeedPhaseGenScreenProps) {
+  const generateSeedPhaseOnClickHandler = () => {
+    setSeedPhase(seedPhaseGen().mnemonic);
+    localStorage.setItem("seed-phase", seedPhaseGen().mnemonic);
+  };
+  return (
+    <div className="flex flex-col gap-2 mx-4 md:mx-12 lg:mx-20 my-12 text-sm">
+      <h1 className="font-extrabold text-2xl md:text-4xl ">
+        Secret Recovery Phrase
+      </h1>
+      <p className="opacity-60">Save these words in a safe place.</p>
+      <div className="flex gap-2 items-center">
+        <Field>
+          <FieldLabel htmlFor="seedphase-input">Secret phase </FieldLabel>
+          <Input
+            id="seedphase-input"
+            type="text"
+            placeholder="Enter your secret phase"
+          />
+          <FieldDescription>
+            Leave it blank to generate a new one
+          </FieldDescription>
+        </Field>
+        <Button
+          onClick={generateSeedPhaseOnClickHandler}
+          className="cursor-pointer"
+        >
+          Generate
+        </Button>
+      </div>
+    </div>
+  );
+}
